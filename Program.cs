018c18fda9d1ab85d2bac8e6a32c26c7d97a5765
open_x <open_x@inbox.ru>
2022-09-13 00:54:01 +0300

    создал прогу

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine ();
+
+
+Console.WriteLine("Таблица умножения:");
+
+Console.WriteLine ();
+for (int i = 2; i <= 10; i++)
+{
+    Console.WriteLine($"Умножение на {i}");
+    Console.WriteLine();
+
+    for (int j = 2; j <=10; j++)
+        {
+            Console.WriteLine($"{i} X {j} = {i*j}");
+        }
+        Console.WriteLine ();
+    
+}
